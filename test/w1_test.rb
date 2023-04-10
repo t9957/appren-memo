ce2d1675dd60af1edebd47353a70fc8ad509d0e2
Takeharu Narita <t995771oxff@gmail.com>
2023-04-11 07:54:03 +0900

    update w1_test

--- test/w1_test.rb
+++ test/w1_test.rb
@@ -1,4 +1,6 @@
 require "minitest/autorun"
+
+# Document https://docs.ruby-lang.org/ja/latest/class/Dir.html
 Dir[File.dirname(__FILE__) + "/../quest/w1q5*.rb"].each {|file| require file}
 
 class W1Test < Minitest::Test
